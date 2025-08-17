fe71a0a203a1389284131977010fa544f38a623e
DM3 <dmitry004@mail.ru>
2025-08-17 07:38:53 +0300

    improved urls in app_run

--- app_run/urls.py
+++ app_run/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import *
+from app_run.views import get_company_details
 
 urlpatterns = [
     path("company_details/", get_company_details, name="get_company_details"),
