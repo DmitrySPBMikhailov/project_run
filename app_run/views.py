f046e9ce074b69b3087998daa1cd32212fd1529c
DM3 <dmitry004@mail.ru>
2025-08-20 10:54:37 +0300

    added ordering Run and User and some filters

--- app_run/views.py
+++ app_run/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination
 from django.conf import settings
 from rest_framework import viewsets
 from rest_framework.views import APIView
@@ -10,6 +11,7 @@ from django.contrib.auth.models import User
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 @api_view(["GET"])
@@ -18,6 +20,11 @@ def get_company_details(request):
     Sends main information about the company.
     """
     return Response(settings.COMPANY_INFORMATION)
+
+
+class AppPagination(PageNumberPagination):
+    page_size_query_param = "size"
+    max_page_size = 50
 
 
 class RunViewSet(viewsets.ModelViewSet):
@@ -29,6 +36,11 @@ class RunViewSet(viewsets.ModelViewSet):
 
     queryset = Run.objects.select_related("athlete").all()
     serializer_class = RunSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ["status", "athlete"]
+    ordering_fields = ["created_at"]
+    ordering = ["id"]
+    pagination_class = AppPagination
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
@@ -49,8 +61,11 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = User.objects.exclude(is_superuser=True)
     serializer_class = UserSerializer
-    filter_backends = [SearchFilter]
+    filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ["first_name", "last_name"]
+    ordering = ["id"]
+    ordering_fields = ["date_joined"]
+    pagination_class = AppPagination
 
     def get_queryset(self):
         param_type = self.request.query_params.get("type")
--- project_run/settings/base.py
+++ project_run/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # libraries
     "rest_framework",
+    "django_filters",
     # users
     "app_run",
 ]
