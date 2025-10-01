0574274fa5bc44c1175d0199adc334cb46738c3f
DM3 <dmitry004@mail.ru>
2025-10-01 12:40:14 +0300

    fix avr speed

--- app_run/views.py
+++ app_run/views.py
@@ -198,15 +198,8 @@ class StopRunView(APIView):
 
             # avarage speed (meters per seconds)
             if run.run_time_seconds > 0:
-                last_position = (
-                    Position.objects.filter(run=run).order_by("-date_time").first()
-                )
-                if last_position and last_position.distance is not None:
-                    total_distance_km = last_position.distance
-                    avg_speed = total_distance_km * 1000 / run.run_time_seconds
-                    run.speed = round(avg_speed, 2)
-                else:
-                    run.speed = 0
+                avg_speed = geodesic(start, finish).meters / result_time.total_seconds()
+                run.speed = round(avg_speed, 2)
             else:
                 run.speed = 0
         else:
